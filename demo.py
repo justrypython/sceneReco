3dc8d7ccd419e04d1390bfc42f6e7bcd78d4ca15
kerzhao <kerzhao@163.com>
2018-02-27 15:31:39 +0800

    update

--- demo.py
+++ demo.py
@@ -13,11 +13,11 @@ print "\ninput exit break\n"
 while 1 :
     im_name = raw_input("\nplease input file name:")
     if im_name == "exit":
-       break
+        break
     im_path = "./img/" + im_name
     im = cv2.imread(im_path)
     if im is None:
-      continue
+        continue
     timer.tic()
     img,text_recs = getCharBlock(text_detector,im)
     crnnRec(model,converter,img,text_recs)
